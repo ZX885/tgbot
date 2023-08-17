05f4d7e356dd4a9b830d6eec45b93014bebb88b8
ZX885 <128841703+ZX885@users.noreply.github.com>
2023-08-17 08:57:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def _send_local_file(update, context):
         RU: Мы должны открыть файл в двоичном режиме,
         иначе Telegram не сможет обработать его правильно
     """
-    with open("image.jfif", "rb") as f:
+    with open("images.jfif", "rb") as f:
         """
                 update.message.reply_photo(photo, caption=None)
             photo   - Photo to send
